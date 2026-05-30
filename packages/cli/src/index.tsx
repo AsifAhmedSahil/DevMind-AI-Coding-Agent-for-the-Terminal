fc7fe0b73d135e0fbbeddc0d1ef6cf90bd728370
Asif Ahmed Sahil <asifahmedsahil.12@gmail.com>
2026-05-30 15:18:07 +0600

    addes keyboard layer provider

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -3,15 +3,18 @@ import { createRoot } from "@opentui/react";
 import { Header } from "./components/header";
 import { InputBar } from "./components/input-bar";
 import { ToastProvider } from "./providers/toast";
+import { KeyboardLayerProvider } from "./providers/keyboard-layer";
 
 function App() {
   return (
+    <KeyboardLayerProvider>
     <ToastProvider>
       <box alignItems="center" justifyContent="center" flexGrow={1}>
         <Header />
         <InputBar onSubmit={() => {}} />
       </box>
     </ToastProvider>
+    </KeyboardLayerProvider>
   );
 }
 
